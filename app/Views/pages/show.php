448c48e499528affd1571ae9c2c916e252346501
Arun Biradar <arunenve@gmail.com>
2026-07-13 05:59:31 +0530

    Fixed home page

--- app/Views/pages/show.php
+++ app/Views/pages/show.php
@@ -3,9 +3,10 @@
     <?php require __DIR__ . '/../partials/header.php'; ?>
     <?= $page['content'] ?>
     <?php require __DIR__ . '/../partials/footer.php'; ?>
-<script type="text/javascript">
-    console.log('fe-php/app/Views/layouts/main.php')
-</script>
+
+    <script type="text/javascript">
+        console.log('fe-php/app/Views/layouts/show.php')
+    </script>
 
     <script src="https://cdn.cpx-research.com/assets/js/script_tag_v2.0.js"></script>
     <script src="/assets/js/theme.js"></script>
